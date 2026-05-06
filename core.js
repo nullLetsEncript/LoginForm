85fb700265dec7c9220c09411ac779bc5dea8803
KingNull <nullletsencript@gmail.com>
2026-05-06 13:26:58 +0330

    prepare background of project

---
+++ core.js
@@ -0,0 +1,23 @@
+// get control
+const sections = [document.getElementById("1"), document.getElementById("2"), document.getElementById("3")];
+const SecBtns = [document.getElementById("sec1"),document.getElementById("sec2"), document.getElementById("sec3")];
+
+// prepare
+let nowSec = 0;
+const displayType = "block";
+function setSectionStat() {
+    for (let i = 0; i < sections.length; i++) {
+        if (nowSec === i) {
+            sections[i].style['display'] = displayType;
+        } else {
+            sections[i].style['display'] = "none";
+        }
+    }
+}
+setSectionStat();
+for (let i = 0; i < SecBtns.length; i++) {
+    SecBtns[i].onclick = () => {
+        nowSec = i;
+        setSectionStat();
+    }
+}
